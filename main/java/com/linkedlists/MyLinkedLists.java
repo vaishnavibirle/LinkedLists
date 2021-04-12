9154ff7a425b01e50227d2b510f43aaaf05ba39e
vaishnavi birle <vaishnavibirle1133@gmail.com>
2021-04-12 23:40:43 +0530

    [vaishnavi] : added welcome message

---
+++ main/java/com/linkedlists/MyLinkedLists.java
@@ -0,0 +1,8 @@
+package com.linkedlists;
+
+public class MyLinkedLists {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to my linked list program");
+    }
+}
---
+++ test/java/com/linkedlists/MyLinkedListsTest.java
@@ -0,0 +1,4 @@
+package com.linkedlists;
+
+public class MyLinkedListsTest {
+}
